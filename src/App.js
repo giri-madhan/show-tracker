1bf36b91cda982d955e0abc8ddf3a8e50b5dc007
MichaelWhitt <michaelwhitt90@gmail.com>
2022-05-22 12:04:20 -0500

    feat(app): set up provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Show Tracker
-      </header>
+      Show Tracker
     </div>
   );
 }
