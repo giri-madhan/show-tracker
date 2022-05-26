266ef8b32191a53e3b6d5e5286e59c9b8eef15dd
MichaelWhitt <michaelwhitt90@gmail.com>
2022-05-26 16:57:55 -0500

    fix(entry): add list and view toggle ui

--- src/features/Dashboard/Entries/EntryContainer.js
+++ src/features/Dashboard/Entries/EntryContainer.js
@@ -6,6 +6,13 @@ export default class EntryContainer extends React.Component {
         return(
             <div style={{width: '80%', height: '100%', border: '1px solid black'}}>
                 Entry Container
+                <div style={{display: 'flex', gap: 40}}>
+                <div style={{display: 'flex',}}>
+                    <button>List View</button>
+                    <button>Card View</button>
+                </div>
+                <h1>Watch List</h1>
+                </div>
             </div>
         )
     }
