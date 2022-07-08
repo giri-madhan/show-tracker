e9daabd6eb3b016aa33d4cc76e171043bbe46c1c
MichaelWhitt <michaelwhitt90@gmail.com>
2022-07-07 21:15:25 -0500

    fix(app): remove unused code

--- src/features/Dashboard/MainView/WatchedList/WatchedEntryCard.js
+++ src/features/Dashboard/MainView/WatchedList/WatchedEntryCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import defaultPoster from '../../../../icons/default_poster.jpg'
 
 const WatchedEntryCard = ({mp, cardView}) => {
