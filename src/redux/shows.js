4d32791ef0f26ac62f8c700a2694f6b33525ed7c
MichaelWhitt <michaelwhitt90@gmail.com>
2022-07-14 17:09:10 -0500

    fix(app): remove comment

--- src/redux/shows.js
+++ src/redux/shows.js
@@ -5,6 +5,11 @@ import axios from 'axios'
 //  return res.data
 // })
 
+// Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+
 export const getMPs = createAsyncThunk('mps/getMPs', async () => {
   return axios.get('/api/getMP').then(res => {
     return res.data
