cdadf06ba1cf7621905e239eb5b8728cfb053d7d
MichaelWhitt <michaelwhitt90@gmail.com>
2022-07-15 19:32:10 -0500

    fix(store): remove counter slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
-import counterReducer from './counter'
+
 import mpReducer from './shows'
 import wliReducer from './watchlist'
 //new reducer goes here
 
 export default configureStore({
     reducer: {
-        counter: counterReducer,
         mps: mpReducer,
         wlis: wliReducer
     }
