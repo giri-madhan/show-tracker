44c1d0d2923da5c507f1e0ee0fb484c357c6acde
Michael Whitt <michaelwhitt90@gmail.com>
2022-11-21 11:37:16 -0600

    fix watched card formatting and add watch date to card

--- src/features/Dashboard/MainView/WatchedList/WatchedEntryCard.js
+++ src/features/Dashboard/MainView/WatchedList/WatchedEntryCard.js
@@ -4,7 +4,6 @@ import defaultPoster from '../../../../icons/default_poster.jpg'
 const WatchedEntryCard = ({mp}) => {
     const posterPath = 'https://image.tmdb.org/t/p/original'
     
-    
     return(
         <>
             <div className='robust-watched-entry-card'>
@@ -14,15 +13,20 @@ const WatchedEntryCard = ({mp}) => {
                         width={66} 
                         style={{borderRadius: '5px 0 0 5px'}} />
                 </div>
-                <div style={{width:'50%', minWidth: 250}}>
-                <div style={{fontSize: 27, color: 'rgb(241, 100, 100)', marginTop: 7}}>
-                    {mp.name}
-                    <span style={{color: '#aaa', marginLeft: 20, fontSize: 20}}>{`(${mp.releaseDate.substring(0,4)})`}</span>
+                <div style={{flex: 4, minWidth: 250}}>
+                    <div style={{fontSize: 27, color: 'rgb(241, 100, 100)', marginTop: 7}}>
+                        {mp.name}
+                        <span style={{color: '#aaa', marginLeft: 20, fontSize: 20}}>{`(${mp.releaseDate.substring(0,4)})`}</span>
+                    </div>
+                    <div style={{marginTop: 15}}>
+                        {mp.genre}
+                    </div>
+                    <div>
+                        {mp.duration} mins
+                    </div>
                 </div>
-                    <div style={{marginTop: 15}}>{mp.genre}</div>
-                    <div>{mp.duration} mins</div>
-                </div>    
-                <div style={{fontSize: 40, width:'40%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <div className='c' style={{flex: 1}}>{mp.watchDate}</div>
+                <div className='c' style={{fontSize: 40, flex: 1, height: '100%'}}>
                     <div>{mp.rating}/10</div>
                 </div>
             </div>
