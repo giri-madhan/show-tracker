a3afc75a3c6f2117035ba0a9456a646c2e3f21ef
Michael Whitt <michaelwhitt90@gmail.com>
2022-11-21 10:58:07 -0600

    add todos in appjs

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import Home from './features/Dashboard/Dashboard'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
+//TODO Watched List Filters / Reverse Order
+//TODO Image rendering problem
+//TODO Change conditionally render after adding comment
+//TODO tabIndex -1 on clear search btn
+//TODO Accounts or public/private at least
+//TODO (OPTIONAL) Add a different way to visualize watched entries
 
 
 function App() {
-  
   return (
     <div className="App">
       <ToastContainer
