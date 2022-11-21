e13fe54a07dd0128e2b023223fd5fd28dbfe5a9b
Michael Whitt <michaelwhitt90@gmail.com>
2022-11-21 12:19:31 -0600

    refactor entry container to function

--- src/features/Dashboard/MainView/WatchedList/WatchedEntryContainer.js
+++ src/features/Dashboard/MainView/WatchedList/WatchedEntryContainer.js
@@ -1,30 +1,28 @@
 import React from 'react'
 import WatchedEntryCard from './WatchedEntryCard'
 
-export default class EntryContainer extends React.Component {
+const EntryContainer = (props) => {
+    const {changeDisplay, view, mpData} = props
 
-    render(){
-        const {changeDisplay, view} = this.props
-
-        return(
-            <div className='watched-entry-container'>
-                <div style={{display: 'flex', height: 80, alignItems: 'center', padding: '10px 0', margin: '0 50px'}}>
-                    <h1 style={{color: '#fff'}}>
-                        {view === 'watched' ? 'Watched History' : null}
-                    </h1>
-                    <button 
-                        className='change-view-btn'
-                        style={{marginLeft: 'auto'}}
-                        onClick={changeDisplay}>
-                            {view === 'watched' ? 'View Watch List' : null}
-                    </button> 
-                </div>
-               <div>
-                   {this.props.mpData.slice(0).reverse().map( (mp, i) => (
-                       <WatchedEntryCard mp={mp} key={i}  />
-                   ))}
-               </div>
+    return(
+        <div className='watched-entry-container'>
+            <div style={{display: 'flex', height: 80, alignItems: 'center', padding: '10px 0', margin: '0 50px'}}>
+                <h1 style={{color: '#fff'}}>
+                    {view === 'watched' && 'Watched History'}
+                </h1>
+                <button 
+                    className='change-view-btn'
+                    style={{marginLeft: 'auto'}}
+                    onClick={changeDisplay}>
+                        {view === 'watched' && 'View Watch List'}
+                </button> 
             </div>
-        )
-    }
+            <div>
+                {mpData.slice(0).reverse().map((mp, i) => (
+                    <WatchedEntryCard mp={mp} key={i}  />
+                ))}
+            </div>
+        </div>
+    )
 }
+export default EntryContainer
--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,4 @@
 const formatDate = (date) => {
-    console.log(date)
     const year = date.slice(0,4)
     const month = date.slice(5,7)
     const day = date.slice(8,10)
