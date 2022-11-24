16464e2a2b80ee83884551216448be733345593b
Michael Whitt <michaelwhitt90@gmail.com>
2022-11-24 11:16:07 -0600

    remove todo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import LandingPage from './features/Landing/Landing';
 //TODO Accounts or public/private at least
 //TODO Fix fail toast for already in watch list
 //TODO Mobile View
-//TODO Color-code watched entry rating
 //TODO (OPTIONAL) Add a different way to visualize watched entries
 //TODO (Optional) Watched List Filters / Reverse Order
 
