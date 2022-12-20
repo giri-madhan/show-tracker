bedf4ac223735791e6150276c4c986c80bbbe211
Michael Whitt <michaelwhitt90@gmail.com>
2022-12-20 13:50:02 -0600

    fix send query

--- functions/utilities/sendQuery.js
+++ functions/utilities/sendQuery.js
@@ -16,7 +16,7 @@ module.exports = async (query, variables) => {
     })
     if (errors) {
         console.log(errors)
-        throw new Error('Something went wrong')
+        throw new Error('Something went wrong', errors)
     }
     return data
 
