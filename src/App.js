2ecff59f33d76936181612ae240324fd2d533cb8
Michael Whitt <michaelwhitt90@gmail.com>
2022-12-21 14:44:40 -0600

    fix(app): add todos

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import LandingPage from './features/Landing/Landing';
 import { useAuth0 } from '@auth0/auth0-react'
 
-//TODO Accounts or public/private at least
-//TODO Mobile View
-//TODO Add scrolling all containers as necessary+
-//TODO Check UI for responsiveness, add breakpoints as needed
+//TODO Accounts or public/private at least -> guest data or separate accounts
+//TODO Mobile / Responsive View
+//TODO Add scrolling all containers as necessary (search container scrolls right, check others)
 //TODO Clean up unused code
 //TODO lazy loading
-//TODO (OPTIONAL) Add a different way to visualize watched entries...chart with genre / ratings / duration etc?
+//TODO refactor weCard setState logic for data object....dont need state here
+//TODO Add a different way to visualize watched entries...chart with genre / ratings / duration etc? chartjs or my own + animation
 //TODO (Optional) Watched List Filters / Reverse Order
 
 function App() {
