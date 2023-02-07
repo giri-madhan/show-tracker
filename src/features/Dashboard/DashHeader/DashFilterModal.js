f8dbca53cd9820ac5d6d6beef3b65a4c022c2734
Michael Whitt <michaelwhitt90@gmail.com>
2023-02-06 22:41:28 -0600

    fix(dash-filter): add animation and mobile css

--- src/features/Dashboard/DashHeader/DashFilterModal.js
+++ src/features/Dashboard/DashHeader/DashFilterModal.js
@@ -2,11 +2,18 @@ import Modal from 'react-modal'
 import GenreFilterBtns from '../MainView/WatchList/GenreFilterButtons'
 
 const DashFilterModal = (props) => {
-    const {isOpen, toggle, watchListItems, setFilterList} = props//
+    const {isOpen, toggle, watchListItems, setFilterList} = props
 
     return (
-        <Modal isOpen={isOpen} className='watch-list-modal' overlayClassName='watch-list-modal-overlay' ariaHideApp={false}>
-            <div style={{display: 'flex', background: '#313131', height: 50, alignItems: 'center' }}>
+        <Modal 
+            isOpen={isOpen} 
+            className='watch-list-modal' 
+            overlayClassName='watch-list-modal-overlay' 
+            shouldCloseOnOverlayClick={true}
+            onRequestClose={toggle} 
+            ariaHideApp={false}
+        >
+            <div style={{display: 'flex', background: '#313131', height: 50, alignItems: 'center', marginBottom: 10 }}>
                 <span style={{ color: '#fff', fontWeight: 700, fontSize: 26, margin:'0 auto'}}>Select Filter</span>
                 <button className='close-filter-btn' onClick={() => toggle(!isOpen)}>X</button>
             </div>
