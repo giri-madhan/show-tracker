e3eb338723f259e2c38dc1974017734c37560578
Michael Whitt <michaelwhitt90@gmail.com>
2023-02-11 10:45:34 -0600

    fix(create-card): fix create card mobile

--- src/features/Dashboard/Create/CreateCard.js
+++ src/features/Dashboard/Create/CreateCard.js
@@ -14,15 +14,15 @@ const CreateCard = ({setRating, watchDate, setWatchDate, notes, setNotes}) => {
     return(
         <div className='center' id='create-card-container' >
             <div id='create-card-top-row'>
-                <div className='center' style={{height: 50}}>
+                <div className='center'>
                     <span style={{color: '#ddd', fontSize: 14, flex: 1}}>Watch Date: </span>
-                    <input style={{borderRadius: 15, height: 50, flex: 2}} type='date' value={watchDate || moment().format('MM.dd.yyyy')} onChange={e => setWatchDate(e.target.value)}/>
+                    <input style={{borderRadius: 5, height: 50, flex: 2}} type='date' value={watchDate || moment().format('MM.dd.yyyy')} onChange={e => setWatchDate(e.target.value)}/>
                 </div>
                 <div className='center'>
                     <span style={{color: '#ddd', fontSize: 14, flex: 1}}>Rating: </span>
-                    <select style={{height: 50, borderRadius: 15, flex: 2 }} onChange={(e) => setRating(e.target.value)}>
+                    <select style={{height: 50, borderRadius: 5, flex: 2 }} onChange={(e) => setRating(e.target.value)}>
                     {generateOptions()}
-                </select>
+                    </select>
                 </div>
             </div>
             <div id='create-card-bottom-row'>
