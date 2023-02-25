a786e1ba38b9f60b471dc7c89b9545843ba99113
Michael Whitt <michaelwhitt90@gmail.com>
2023-02-25 11:40:50 -0600

    fix(app): remove todos

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import LandingPage from './features/Landing/Landing';
 import { useAuth0 } from '@auth0/auth0-react'
 
 //TODO Accounts or public/private at least -> guest data or separate accounts
-//TODO Mobile / Responsive View
 //TODO Add profile page, nest buttons inside dropdown w/ email/name/img as icon
 //TODO add to top button on wl, watched
 //TODO if movie exists in watched, let user choose to add again; watchCount + 1 instead of new watched instance?
 //TODO Clean up unused code
 //TODO convert class components to func components
 //TODO lazy loading
-//TODO refactor weCard setState logic for data object....dont need state here
 //TODO Add a different way to visualize watched entries...chart with genre / ratings / duration etc? chartjs or my own + animation
 //TODO (Optional) Watched List Filters / Reverse Order
 
