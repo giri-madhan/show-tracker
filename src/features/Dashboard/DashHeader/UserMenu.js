cc94de915a5f487c4300a54258a7c3897c2c0309
Michael Whitt <michaelwhitt90@gmail.com>
2023-03-04 09:34:51 -0600

    feat(user): add user profile information

--- src/features/Dashboard/DashHeader/UserMenu.js
+++ src/features/Dashboard/DashHeader/UserMenu.js
@@ -1,14 +1,16 @@
 import {useState} from 'react'
 import Modal from 'react-modal'
 import LogoutButton from '../../Buttons/LogoutButton'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const UserMenu = (props) => {
-    const {
-        user = {
-            email: '',
-            name: ''
-        }
-    } = props
+    const {user} = useAuth0()
+    const getInitials = () => {
+        const firstInitial = user?.given_name?.slice(0,1)
+        const lastInitial = user?.family_name?.slice(0,1)
+
+        return firstInitial + lastInitial
+    }
 
     const [userModalOpen, setUserModalOpen] = useState(false)
 
@@ -19,7 +21,13 @@ const UserMenu = (props) => {
             style={userModalOpen ? {background: '#9FE2BF', color: '#313131'} : null} 
             onClick={() => setUserModalOpen(!userModalOpen)}
         >
-            {user.name || 'MW'}
+            {user?.picture ? (
+                <img src={user.picture} width={50} height={50} style={{borderRadius: 50}} alt='' />
+            ) : (
+                <div>
+                    {getInitials() || 'P'}
+                </div>
+            )}
             <Modal shouldCloseOnOverlayClick={true} isOpen={userModalOpen} className={`user-menu-modal ${userModalOpen ? 'transition-open' : null}`} overlayClassName='user-menu-modal-overlay' ariaHideApp={false}>
                 <LogoutButton />
             </Modal>
