89d95a284a85df3a8adb4bf01852ebf3d8d5d6a9
Michael Whitt <michaelwhitt90@gmail.com>
2023-03-18 12:15:06 -0500

    feat(landing): add background image

--- src/features/Landing/Landing.js
+++ src/features/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import backgroundImg from '../../icons/pxfuel.jpg'
 
 const LandingPage = () => {
     //TODO Add background
@@ -11,7 +12,15 @@ const LandingPage = () => {
     }
 
 return(
-    <div className='center' style={{width: '100vw', height: '100%', background: '#222', flexDirection: 'column'}}>
+    <div 
+        className='center' 
+        style={{
+            width: '100vw', 
+            height: '100%', 
+            flexDirection: 'column',
+            backgroundImage: `url(${backgroundImg})`
+        }}
+    >
         <div style={{fontSize: 40, color: '#fff', marginBottom: 20}}>MW Movie Tracker</div>
         <button className='auth-btn' onClick={authenticate}>Authenticate</button>
     </div>
