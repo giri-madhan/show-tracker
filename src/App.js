35825fbea8fc2cc5ba818623496665ae25674d92
Michael Whitt <michaelwhitt90@gmail.com>
2023-04-08 17:18:25 -0500

    fix(stat-info): remove excess padding

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
   const {loginWithRedirect, user, isAuthenticated} = useAuth0()
+  //console.log('user', user)
 
   return (
     <div className="App">
