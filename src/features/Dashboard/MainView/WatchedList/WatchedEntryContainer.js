08b2f8f41525df8a8fca1b2c4cae763148bf8dcc
Michael Whitt <michaelwhitt90@gmail.com>
2023-04-13 18:41:10 -0500

    fix(watched-entry-container): remove unused code

--- src/features/Dashboard/MainView/WatchedList/WatchedEntryContainer.js
+++ src/features/Dashboard/MainView/WatchedList/WatchedEntryContainer.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import WatchedEntryCard from './WatchedEntryCard'
 import DashHeader from '../../DashHeader/DashHeader'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const EntryContainer = (props) => {
     const {setViewDisplay, viewDisplay} = props
-    const dispatch = useDispatch()
     const watchedItems = useSelector(state => state.mps.list[0]?.watchedList?.data)
     
 
